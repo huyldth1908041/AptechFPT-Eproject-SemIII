7f89c72d77233325c9a8c2aef21e418966758525
LuuHuy04052001 <LuuHuy04052001>
2021-03-29 07:50:01 +0700

    complete purchase order function

--- VehicleShowRoomManager/VehicleShowRoomManager/Controllers/ShowRoomController.cs
+++ VehicleShowRoomManager/VehicleShowRoomManager/Controllers/ShowRoomController.cs
@@ -28,7 +28,7 @@ namespace VehicleShowRoomManager.Controllers
         {
             //handels img
             //if user dont upload any image use a place holder instead
-            if(Covers == null || Covers.Count() == 0)
+            if (Covers == null || Covers.Count() == 0)
             {
                 //set cover to holder image public key
                 model.Cover = "n2ssze3joengkhuzgzr3";
@@ -64,11 +64,75 @@ namespace VehicleShowRoomManager.Controllers
 
             return RedirectToAction("Index", "Home");
         }
-    
+        //for ajax call only
+
         public ActionResult ListModelsByBrands(int id)
         {
             var list = _db.Brands.Find(id).VehicleModels.ToList();
             return PartialView(list);
+        }
+        public ActionResult CreatePurchaseOrder()
+        {
+            ViewBag.Models = _db.VehicleModels.ToList();
+            ViewBag.Brands = _db.Brands.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePurchaseOrder(PurchaseOrderDetail purchaseOrderDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(purchaseOrderDetail);
+            }
+            //create new order
+            var order = new PurchaseOrder()
+            {
+                Quantity = purchaseOrderDetail.Quantity,
+                TotalPrice = purchaseOrderDetail.Price * purchaseOrderDetail.Quantity,
+                Status = PurchaseOrder.PurchaseOrderStatus.Pending,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+
+            };
+            _db.PurchaseOrders.Add(order);
+            _db.SaveChanges();
+            //save new order detail to this order
+            purchaseOrderDetail.PurchaseOrderId = order.Id;
+    
+            purchaseOrderDetail.CreatedAt = DateTime.Now;
+            purchaseOrderDetail.UpdatedAt = DateTime.Now;
+            purchaseOrderDetail.Status = PurchaseOrderDetail.PurchaseOrderDetailStatus.Pending;
+            _db.PurchaseOrderDetails.Add(purchaseOrderDetail);
+            _db.SaveChanges();
+            return RedirectToAction("ListPurchaseOrder");
+
+        }
+
+        public ActionResult ListPurchaseOrder()
+        {
+            var list = _db.PurchaseOrders.ToList();
+            if(list == null)
+            {
+                return View(new List<PurchaseOrder>());
+            }
+            return View(list);
+        }
+
+        public ActionResult ListPurchaseOrderDetail(int id)
+        {
+            var currentPurchaseOrder = _db.PurchaseOrders.Find(id);
+            if(currentPurchaseOrder == null)
+            {
+                return View(new List<PurchaseOrderDetail>());
+            }
+            var list = currentPurchaseOrder.PurchaseOrderDetails.ToList();
+           
+            if(list == null)
+            {
+                return View(new List<PurchaseOrderDetail>());
+            }
+            return View(list);
         }
     }
 }
